6c94a8df4e4925cfa7dc80ba31a229f1d7352742
yad615 <yadhira.alcantara@tecsup.edu.pe>
2025-12-10 08:33:59 -0500

    terminado

--- backend/authentication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_active', 'created_at')
-    list_filter = ('is_active', 'created_at')
-    search_fields = ('username', 'email')
-    ordering = ('-created_at',)
--- backend/chatbot_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('', views.ChatbotAPIView.as_view(), name='chatbot_api'),
-    path('crop-data/', views.CropDataAPIView.as_view(), name='crop_data_api'),
-
-]
